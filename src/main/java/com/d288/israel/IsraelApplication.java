4ea101a747808045f48db539ee459fce0b351ae2
israel_osorio <iosori6@wgu.edu>
2025-01-02 14:35:43 -0800

    Initial commit

---
+++ src/main/java/com/d288/israel/IsraelApplication.java
@@ -0,0 +1,13 @@
+package com.d288.israel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsraelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsraelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/d288/israel/IsraelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d288.israel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsraelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
