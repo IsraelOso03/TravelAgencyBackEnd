cf913c1655d889b9be294282dba8858174316221
israel_osorio <iosori6@wgu.edu>
2025-01-03 21:36:11 -0800

    Wrote code for the dao package that includes repository interfaces for the entities that extend JpaRepository, and added cross-origin support.

---
+++ src/main/java/com/d288/israel/dao/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.d288.israel.dao;
+
+import com.d288.israel.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:4200")
+public interface CustomerRepository extends JpaRepository <Customer, Long> {
+}
